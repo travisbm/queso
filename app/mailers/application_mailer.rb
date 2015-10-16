10805c329719430fff1de291008e3a7b19e87f57
Travis Montgomery <travisbm@gmail.com>
2015-10-16 13:35:04 -0500

    mailer working on heroku. updating email content.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "from@my-queso.com"
   layout 'mailer'
 end
--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,9 +1,9 @@
 class FavoriteMailer < ApplicationMailer
-  default from: 'notifications@example.com'
+  default from: 'notifications@my-queso.com'
 
   def favorite_email(user)
     @user = user
-    @url  = 'http://example.com/login'
+    @url  = 'https://my-queso.herokuapp.com'
     mail(to: @user.email, subject: 'You like cheese!')
   end
 end
