84118d5c63f2462b5986c425c74e02133d0dcbe8
Travis Montgomery <travisbm@gmail.com>
2015-10-16 13:16:37 -0500

    took out ajax script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,21 +18,21 @@
 
 $(document.ready(function () {
 
-  $('#cheeseLike').on('click', function () {
-    $.ajax({
-      url: '/favorites',
-      dataType: 'JSON',
-      method: "POST",
-      data: {
-        favorite: {
-          user_id: $('#favorite_cheese_id').val(),
-          cheese_id: $('#favorite_user_id').val(),
-        }
-      },
-      success: function (response) {
-        $('#cheese').append('<h4>Cheese Liked!</h4>');
-      }
-    });
-  });
+  // $('#cheeseLike').on('click', function () {
+  //   $.ajax({
+  //     url: '/favorites',
+  //     dataType: 'JSON',
+  //     method: "POST",
+  //     data: {
+  //       favorite: {
+  //         user_id: $('#favorite_cheese_id').val(),
+  //         cheese_id: $('#favorite_user_id').val(),
+  //       }
+  //     },
+  //     success: function (response) {
+  //       $('#cheese').append('<h4>Cheese Liked!</h4>');
+  //     }
+  //   });
+  // });
   
 });
